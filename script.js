1f54f45891fb2fb77271b451355fd32535e65db7
Israelclinton <mosesolamide572@gmail.com>
2024-05-28 12:02:30 +0100

    commit

---
+++ script.js
@@ -0,0 +1,135 @@
+'use strict';
+
+/*
+const friend1 = 'Michael';
+const friend2 = 'David';
+const friend3 = 'Aina';
+
+const friends = ['Michael', 'David', 'Aina'];
+console.log(friends);
+
+const years = new Array(1991, 1984, 2008, 2020);
+
+console.log(friends[0]);
+console.log(friends[2]);
+
+console.log(friends.length);
+console.log(friends[friends.length - 1]);
+
+friends[2] = 'Jay';
+console.log(friends);
+// friends = ['Bob', 'Alice']
+
+const firstName = 'Israel';
+const Israel = [firstName, 'Moses', 2037 - 2000, 'teacher, friends'];
+console.log(Israel);
+console.log(Israel.length);
+
+//Exercise
+const calcAge = function (birthYear) {
+    return 2037 - birthYear;
+}
+const years1 = [1990, 1967, 2002, 2010, 2018];
+
+const age1 = calcAge(years1[0]);
+const age2 = calcAge(years1[1]);
+const age3 = calcAge(years1[years1.length - 1]);
+console.log(age1, age2, age3);
+
+const ages = [calcAge(years1[0]), calcAge(years1[1])]; calcAge(years1[years1.length - 1]);
+console.log(ages);
+
+// console.log(calcAge(years1));
+*/
+
+/*
+const friends = ['Michael', 'David', 'Aina'];
+
+// Add elements
+const newLength = friends.push('Jay');
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('Abdul');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+const popped = friends.pop();
+console.log(friends);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('David'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(23);
+console.log(friends.includes('David'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('24'));
+
+if (friends.includes('David')) {
+    console.log('You have a friend called Steven');
+}
+*/
+/*
+const friends = ['Michael', 'David', 'Aina'];
+
+// Add elements
+friends.push('Jay');
+console.log(friends);
+// console.log(newLength);
+
+friends.unshift('Israel');
+console.log(friends);
+
+// Remove elements
+friends.pop(); // Last
+friends.pop();
+// console.log(popped);
+console.log(friends);
+
+friends.shift(); // First
+console.log(friends);
+
+console.log(friends.indexOf('David'));
+console.log(friends.indexOf('Bob'));
+
+friends.push(24)
+console.log(friends.includes('David'));
+console.log(friends.includes('Bob'));
+console.log(friends.includes('24'));
+
+if (friends.includes('David')) {
+    console.log('You have a friend called Aina');
+} else {
+    console.log(`You don't have a friend called Aina`)
+}
+*/
+
+////////////////////////////////
+// Coding Challenge #2
+
+/*
+const calcTip = function(bill) {
+    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2 
+}
+
+// const calcTip = bill => bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+
+const bills = [125, 555, 44];
+const tips = [calcTip(bills[0]), calcTip(bills[1]), calcTip(bills[2])];
+const totals =[(bills[0] + tips[0]), bills[1] + tips[1], bills[2] + tips[2]];
+
+console.log(bills, tips, totals);
+*/
+
+const IsraelArray = [
+    'Israel',
+    'Moses',
+    2037 - 2000,
+    'developer'
+    ['Michael', 'David', 'Steven']
+];
